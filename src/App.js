13080997dc6722fe74570968f790b452ad6d133e
Mikita Pilinka <mineralsfree@gmail.com>
2023-10-29 21:47:11 +0100

    added register commodity delivery page + some common components, fixes #2

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {Commodity} from "./Commodity";
 import {Balance} from "./Balance";
 // import {Insert} from "./Insert";
 // import {Datasets} from "./Datasets";
+import {CircularLoader} from "@dhis2/ui";
 
 import {Navigation} from "./Navigation";
 import {useDataQuery} from "@dhis2/app-runtime";
+import {CommodityDeliveryRegistration} from "./components/Delivery/CommodityDeliveryRegistration";
 
 const query = {
-    mikita: {
+    me: {
         resource: 'me',
         params: {
             fields: [
@@ -20,50 +22,25 @@ const query = {
                 'organisationUnits'
             ],
         },
-    },
-    dataSets: {
-        resource: 'dataSets/ULowA8V3ucd',
-        params: {
-            fields: ['dataSetElements[dataElement[id, displayName, categoryCombo[name,id,categoryOptionCombos[name,id]]]']
-        }
-    },
-    organisation: {
-     resource: 'organisationUnits/ImspTQPwCqd',
-        params: {
-         fields: [
-             'dataSets[id, name, displayName]',
-             'users[id, name, displayName]',
-             '*'
-         ]
-        }
     }
 }
-const dataSetsQuery = {
-    dataSets: {
-        resource: 'dataSets'
-    }
-}
-
 function MyApp() {
     const [activePage, setActivePage] = useState("Balance");
-    const me = useDataQuery(query);
-    const dataSets = useDataQuery(dataSetsQuery)
-    if (me.data) {
-        // console.log(me.data);
-    }
-    if (dataSets.data) {
-        // console.log(dataSets.data);
-    }
-
+    const {data, loading, error} = useDataQuery(query);
     function activePageHandler(page) {
         setActivePage(page);
     }
-
+    if (error) {
+        return <span>ERROR: {error.message}</span>
+    }
+    if (loading){
+        return <CircularLoader large/>
+    }
     return (
         <div className={classes.container}>
             <div className={classes.left}>
                 <Navigation
-                    activePage={activePage}
+                    acti  vePage={activePage}
                     activePageHandler={activePageHandler}
                 />
             </div>
@@ -71,6 +48,7 @@ function MyApp() {
                 {activePage === "Balance" && <Balance/>}
                 {/*    {activePage === "Insert" && <Insert/>}*/}
                 {activePage === "Commodity" && <Commodity/>}
+                {activePage === "Delivery" && <CommodityDeliveryRegistration user={data.me}/>}
             </div>
         </div>
     );
