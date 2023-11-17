725efd59e1540f52028fb37bb4b1b89c3a910abe
Mikkel Lundli <mikkel.lundli@hotmail.com>
2023-11-17 20:44:07 +0100

    restructuring, some name changes

--- src/components/CommodityDispense/CommodityDispense.js
+++ src/components/CommodityDispense/CommodityDispense.js
@@ -1,23 +1,10 @@
 import React from "react";
 import { useDataMutation } from "@dhis2/app-runtime";
-import { CircularLoader } from "@dhis2/ui";
-import "./styles.css";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableCellHead,
-  TableHead,
-  TableRow,
-  TableRowHead,
-  Card,
-} from "@dhis2/ui";
-import { formatDatetime } from "./utils/formatting";
-import { useDispenseHistory } from "./hooks/useDispenseHistory";
-import CommodityDispenseForm from "./components/CommodityDispense/CommodityDispenseForm";
+import { useDispenseHistory } from "../../hooks/useDispenseHistory";
+import CommodityDispenseForm from "./CommodityDispenseForm";
+import { DispenseHistoryTable } from "./DispenseHistoryTable";
 import toast, { Toaster } from "react-hot-toast";
-import { ORG_UNIT } from "./consts";
-import * as PropTypes from "prop-types";
+import { ORG_UNIT } from "../../consts";
 
 const dataMutationQuery = {
   dataSet: "ULowA8V3ucd",
@@ -43,7 +30,7 @@ const dispenseHistoryMutationQuery = {
   }),
 };
 
-export function Commodity() {
+export function CommodityDispense() {
   const [mutateCommodities] = useDataMutation(dataMutationQuery);
   const [mutateHistory] = useDataMutation(dispenseHistoryMutationQuery);
 
@@ -124,59 +111,3 @@ export function Commodity() {
     </div>
   );
 }
-
-function DispenseHistoryTable({ useDispenseHistory }) {
-  const { loading, error, dispenseHistory } = useDispenseHistory;
-
-  if (error) {
-    return <span>ERROR: {error.message}</span>;
-  }
-
-  if (loading) {
-    return <CircularLoader large />;
-  }
-
-  if (!dispenseHistory) {
-    return null;
-  }
-
-  // sort dipenseHistory by date
-  const dispenseHistorySorted = dispenseHistory.sort((a, b) => {
-    return new Date(b.dateDispensed) - new Date(a.dateDispensed);
-  });
-
-  return (
-    <Card>
-      <div style={{ padding: "24px" }}>
-        <h3>Commodity dispense history</h3>
-        <Table>
-          <TableHead>
-            <TableRowHead>
-              <TableCellHead>Commodity</TableCellHead>
-              <TableCellHead>Quantity dispensed</TableCellHead>
-              <TableCellHead>Dispensed by</TableCellHead>
-              <TableCellHead>Dispensed to</TableCellHead>
-              <TableCellHead>Date</TableCellHead>
-            </TableRowHead>
-          </TableHead>
-          <TableBody>
-            {dispenseHistorySorted.map((row, i) => {
-              return (
-                <TableRow key={i}>
-                  <TableCell>{row.displayName}</TableCell>
-                  <TableCell>{row.quantityDispensed}</TableCell>
-                  <TableCell>{row.dispensedBy}</TableCell>
-                  <TableCell>{row.dispensedTo}</TableCell>
-                  <TableCell>{formatDatetime(row.dateDispensed)}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </div>
-    </Card>
-  );
-}
-DispenseHistoryTable.propTypes = {
-  useDispenseHistory: PropTypes.objectOf(PropTypes.any),
-};
