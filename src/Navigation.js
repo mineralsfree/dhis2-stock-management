bbde048711d8afc22647de2adb6aa018a7c492f2
KiyaNezif <nezifkiya@gmail.com>
2023-11-17 19:46:35 +0100

    small chage/ delete

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Menu, MenuItem } from "@dhis2/ui";
 
+const activeStyle = {
+  backgroundColor: "#f0f0f0",
+};
+
 export function Navigation(props) {
   return (
     <Menu>
@@ -8,18 +12,23 @@ export function Navigation(props) {
         label="Stock Balance"
         active={props.activePage === "Balance"}
         onClick={() => props.activePageHandler("Balance")}
+        style={props.activePage === "Balance" ? activeStyle : null}
+
       />
      
       <MenuItem
             label="Dispensing Commodity"
             active={props.activePage === "Commodity"}
             onClick={() => props.activePageHandler("Commodity")}
+            style={props.activePage === "Balance" ? activeStyle : null}
+
         />
         <MenuItem
             label="Register Delivery"
             active={props.activePage === "Delivery"}
             onClick={() => props.activePageHandler("Delivery")}
-        />
+            style={props.activePage === "Delivery" ? activeStyle : null}
+            />
     </Menu>
   );
 }
